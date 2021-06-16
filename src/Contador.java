3b50fd96e452771c751f658e41a69587fa628c8c
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-06-15 23:23:04 -0600

    Agregue la carpeta src y el archivo Contador.java

---
+++ src/Contador.java
@@ -0,0 +1,36 @@
+public class Contador {
+
+    // atributos
+    private int contador;
+
+    // constructores
+    public Contador() {
+
+    }
+
+    public Contador(int contador) { // constructor con parametros
+        this.contador = contador;
+    }
+
+    public Contador(Contador copia) { // Constructor copia
+        this.contador = copia.contador;
+    }
+
+    // metodos
+
+    public void setContador(int valor) {
+        this.contador = valor;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void incrementar() {
+        this.contador++;
+    }
+
+    public void decrementar(int cantidad) {
+        this.contador--;
+    }
+}
